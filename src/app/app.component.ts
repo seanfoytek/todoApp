8f9b0a4d0fc06c8c09bef8dcc989c6e68884e4aa
Unknown <seanfoytek@gmail.com>
2018-07-10 15:07:08 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  todoInput: string = "";
+   todos = [{
+     isChecked: false,
+     name:"mow lawn",
+    },
+    {
+      isChecked: true,
+      name:" eat icecream",
+    },
+    {
+      isChecked: false,
+      name:"Watch anime"
+    }
+   ];
+   checkOffTodo(todo) {
+     todo.isChecked = !todo.isChecked;
+   }
+
+  createTodo(){
+    let trimmedInput = this.todoInput.trim();
+  if(this.todoInput != ""){
+  this.todos.push({
+    isChecked: false,
+    name: this.todoInput
+  });
+};
+
+  this.todoInput = "";
+  }
+
+  editTodo(todo){
+    
+    let index = this.todos.indexOf(todo);
+    
+    let oldTodo = this.todos[index].name
+    
+    this.todos[index].name = prompt("edit work", this.todos[index].name);
+
+if (this.todos[index].name == null) {
+  this.todos[index].name = oldTodo;
+} 
+ }
+
+  deleteTodo(todo) {
+    let index = this.todos.indexOf(todo);
+    this.todos.splice(index, 3);
+  }
+
+  deleteAllTodos() {
+    this.todos = [];
+  }
+  
 }
